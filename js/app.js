126ef3123afc2ac006705b563843d2ae45906e50
Aviv-Yaari <avivyar@gmail.com>
2021-08-07 21:45:47 +0300

    added modal

--- js/app.js
+++ js/app.js
@@ -1,16 +1,33 @@
 function onExpandNav() {
   const elNav = document.querySelector('.navbar');
   if (!elNav.classList.contains('expanded')) {
-    elNav.classList.add('expanding');
-    setTimeout(() => {
-      elNav.classList.remove('expanding');
-      elNav.classList.add('expanded');
-    }, 500);
+    elNav.classList.remove('collapsed');
+    elNav.classList.add('expanded');
   } else {
-    elNav.classList.add('collapsing');
-    setTimeout(() => {
-      elNav.classList.remove('expanded');
-      elNav.classList.remove('collapsing');
-    }, 500);
+    elNav.classList.remove('expanded');
+    elNav.classList.add('collapsed');
   }
+}
+
+function onReadMore() {
+  const elModal = document.querySelector('.modal-read-more');
+  if (!elModal.classList.contains('visible')) {
+    elModal.classList.remove('hidden');
+    elModal.classList.add('visible');
+  }
+}
+
+function onModalClose() {
+  const elModal = document.querySelector('.modal-read-more');
+  elModal.classList.remove('visible');
+  elModal.classList.add('hidden');
+}
+
+function onModalSubmit(ev) {
+  ev.preventDefault();
+  onModalClose();
+}
+
+function stopProp(ev) {
+  ev.stopPropagation();
 }
